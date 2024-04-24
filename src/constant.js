cd8d4eff317da83e75401dbe57470ca069da040f
Pablo Jara <pablojaramunoz@icloud.com>
2024-04-24 17:35:45 -0300

    switch server

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-const Url = 'http://localhost:4000/api';
-//const Url = 'https://mecanica-server.onrender.com/api'
+//const Url = 'http://localhost:4000/api';
+const Url = 'https://mecanica-server.onrender.com/api'
 
 
 export { Url }
